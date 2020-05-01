55db07207fd61c103b34f00fa61fbb46b5b8ab02
Chuck Sakoda <chuck@amperity.com>
2020-05-01 14:53:32 -0700

    Fixing and upgrading til it works again

--- project.clj
+++ project.clj
@@ -5,23 +5,24 @@
   :description "A Clojure(Script) Pretty Printer"
   :url "http://pretty-print.net/"
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [cljfmt "0.5.3"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.742"]
+                 [cljfmt "0.6.7"]
                  [secretary "1.2.1"]
-                 [rum "0.10.5"]
-                 [cljsjs/codemirror "5.11.0-2"]
+                 [rum "0.11.5"]
+                 [cljsjs/codemirror "5.44.0-1"]
                  [org.clojure/core.async "0.2.385"]
                  [cljsjs/parinfer "1.8.1-0"]
                  [cljsjs/js-beautify "1.6.2-0"]
                  [cljsjs/clipboard "1.5.9-0"]
-                 [camel-snake-kebab "0.4.0"]]
+                 [camel-snake-kebab "0.4.0"]
+                 [org.clojure/core.async "1.1.587"]]
 
-  :plugins [[lein-cljsbuild "1.1.3"]
+  :plugins [[lein-cljsbuild "1.1.8"]
             [lein-npm "0.6.2"]
             [lein-less "1.7.5"]
-            [lein-figwheel "0.5.4-7"]
-            [lein-asset-minifier "0.3.0"]
+            [lein-figwheel "0.5.19"]
+            [lein-asset-minifier "0.4.6"]
             [lein-pdo "0.1.1"]]
 
   :source-paths ["src"]
@@ -35,9 +36,7 @@
   :figwheel {:css-dirs ["resources/public/css"]
              :repl false}
 
-  :minify-assets {:assets {"resources/public/css/main.min.css"
-                           "resources/public/css/main.css"}
-                  :options {:optimizations :advanced}}
+  :minify-assets [[:css {:source "resources/public/css/main.css" :target "resources/public/css/main.min.css"}]]
 
   :clean-targets ^{:protect false} ["target"
                                     "resources/public/out"
